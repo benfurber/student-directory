1d3a2b3b233a011bd191951362755176c8fe479c
benfurber <ben@furber-consultancy.com>
2018-04-11 07:57:22 +0100

    Added a save to file function

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "Options:"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -20,6 +21,8 @@ def process(input)
     input_students # Change input_students to dumby_students for quick testing
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # This will exit the problem... Like you couldn't have worked that out.
   else
@@ -67,6 +70,20 @@ def input_students
       break
     end
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hero]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+    print "#{student[:name]} added. "
+  end
+  file.close
+  puts
 end
 
 # Called for testing rather than the above method
